3a1f9dde1fe07c0c95cc14345813745b8b8e4d2e
phophobee <imran.abu.hanina@gmail.com>
2025-05-10 15:55:01 +0700

    first push

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('shareBtn').addEventListener('click', function () {
+    const container = document.querySelector('.share-container');
+    container.classList.toggle('active');
+  });
+  
